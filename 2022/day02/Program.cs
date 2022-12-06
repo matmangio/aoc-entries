41b2bddbfd574d3e8fa4715e1175a554c2760232
Matteo Mangioni <mat.mangioni@gmail.com>
2022-12-06 17:58:17 +0100

    Add solution to first star

--- 2022/day02/Program.cs
+++ 2022/day02/Program.cs
@@ -1,8 +1,46 @@
-﻿namespace day02;
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace day02;
+public class Program {
+
+    public static void Main(string[] args) {
+        
+        int myPoints = 0;
+
+        while (!isEOF()) {
+            string line = Console.ReadLine() + "";
+            myPoints += pointsPerRound(line[0], line[2]);
+        }
+
+        Console.WriteLine("Star1: " + myPoints);
+
     }
+
+    private static int pointsPerRound(char opponentMove, char myMove) {
+        int opponentMoveValue = evaluateMove(opponentMove);
+        int myMoveValue = evaluateMove(myMove);
+
+        int points = myMoveValue + 1;
+        if ((opponentMoveValue + 1) % 3 == myMoveValue) {
+            points += 6;
+        } else if (myMoveValue == opponentMoveValue) {
+            points += 3;
+        }
+
+        return points;
+    }
+
+    private static int evaluateMove(char move) {
+        char reference;
+        if (move <= 'C') 
+            reference = 'A';
+        else 
+            reference = 'X';
+        return move - reference;
+    }
+
+    private static bool isEOF() {
+        return Console.In.Peek() == -1;
+    }
+
 }
