10b1c5aeb256de5371dd5931ad2e22d9eaacc45c
Matteo Mangioni <mat.mangioni@gmail.com>
2022-12-06 17:26:51 +0100

    Add solution to first star

--- 2022/day01/Program.cs
+++ 2022/day01/Program.cs
@@ -1,8 +1,33 @@
-﻿namespace day01;
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace day01;
+
+public static class Program {
+
+    static void Main(string[] args) {
+
+        int max = 0;
+        while (!isEOF()) {
+            int current = getNextElfCarry();
+            if (current > max) 
+                max = current;
+        }
+
+        Console.WriteLine("Star1: " + max);
+
+    }
+
+    private static int getNextElfCarry() {
+        int carry = 0;
+        string line = Console.ReadLine() + "";
+        while (!line.Equals("")) {
+            carry += int.Parse(line);
+            line = Console.ReadLine() + "";
+        }
+        return carry;
+    }
+
+    private static bool isEOF() {
+        return Console.In.Peek() == -1;
     }
 }
