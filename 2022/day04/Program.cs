8027f8e51a1e23019d84fea16661d2d9fd237b1e
Matteo Mangioni <mat.mangioni@gmail.com>
2022-12-06 20:08:55 +0100

    Add solution to first star

--- 2022/day04/Program.cs
+++ 2022/day04/Program.cs
@@ -1,8 +1,46 @@
-﻿namespace day04;
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace day04;
+
+class Program {
+
+    record Assignment {
+        public int start;
+        public int end;
+    }
+
+    static void Main(string[] args) {
+
+        int fullContainmentsCounter = 0;
+
+        while (!isEOF()) {
+            string line = Console.ReadLine() + "";
+            if (line.Contains(',')) {
+                string[] assignmentsStrings = line.Split(',');
+                Assignment ass1 = parseAssignement(assignmentsStrings[0]);
+                Assignment ass2 = parseAssignement(assignmentsStrings[1]);
+                if (fullyContains(ass1, ass2) || fullyContains(ass2, ass1))
+                    fullContainmentsCounter++;
+            }
+        }
+
+        Console.WriteLine("Star1: " + fullContainmentsCounter);
+
+    }
+
+    static bool fullyContains(Assignment container, Assignment contained) {
+        return container.start <= contained.start && container.end >= contained.end;
+    }
+
+    static Assignment parseAssignement(string str) {
+        Assignment output = new Assignment();
+        string[] splits = str.Split('-');
+        output.start = int.Parse(splits[0]);
+        output.end = int.Parse(splits[1]);
+        return output;
+    }
+
+    static bool isEOF() {
+        return Console.In.Peek() == -1;
     }
 }
