8c70c4d743535875a44dd85c9d9f7ad2f95925bc
Matteo Mangioni <mat.mangioni@gmail.com>
2022-12-06 19:19:43 +0100

    Add solution to second star

--- 2022/day03/Program.cs
+++ 2022/day03/Program.cs
@@ -4,19 +4,42 @@ namespace day03;
 class Program {
 
     static void Main(string[] args) {
-        int sum = 0;
+        int sumFirstStar = 0, sumSecondStar = 0;
+
+        string[] rucksackGroup = new string[3];
+        int counter = 0;
 
         while (!isEOF()) {
-            sum += priority(findCommonElement(Console.ReadLine() + ""));
+            string rucksack = Console.ReadLine() + "";
+
+            if (rucksack.Length > 0) {
+                sumFirstStar += priority(findCommonElementInCompartments(rucksack));
+
+                rucksackGroup[counter] = rucksack;
+                counter++;
+
+                if (counter >= 3) {
+                    sumSecondStar += priority(findCommonElementInRucksacks(rucksackGroup));
+                    counter = 0;
+                }
+            }
         }
 
-        Console.WriteLine("Star1: " + sum);
+        Console.WriteLine("Star1: " + sumFirstStar);
+        Console.WriteLine("Star2: " + sumSecondStar);
     }
 
-    static char findCommonElement(string rucksack) {
+    static char findCommonElementInCompartments(string rucksack) {
         string firstHalf = rucksack.Substring(0, rucksack.Length / 2);
         string secondHalf = rucksack.Substring(rucksack.Length / 2, rucksack.Length / 2);
         return firstHalf.Intersect(secondHalf).First();
+    }
+
+    static char findCommonElementInRucksacks(string[] rucksacks) {
+        IEnumerable<char> intersect = rucksacks[0];
+        for (int i = 1; i < 3; i++)
+            intersect = intersect.Intersect(rucksacks[i]);
+        return intersect.First();
     }
 
     static int priority(char item) {
