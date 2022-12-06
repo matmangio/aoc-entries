f0d98fc788b856727211bddffc4c809e2e171aac
Matteo Mangioni <mat.mangioni@gmail.com>
2022-12-07 00:03:29 +0100

    Add solution to first star

--- 2022/day06/Program.cs
+++ 2022/day06/Program.cs
@@ -5,8 +5,21 @@ class Program {
 
     static void Main(string[] args) {
 
-        Console.WriteLine("Hello, World!");
+        string input = Console.ReadLine() + "";
 
+        int endIndex = 3;
+        for (; ContainsDuplicates(input.Substring(endIndex - 3, 4)); endIndex++);
+
+        Console.WriteLine("Star1: " + (endIndex + 1));
+
+    }
+
+    static bool ContainsDuplicates(string input) {
+        foreach (char c in input) {
+            if (input.Where(c.Equals).Count() > 1)
+                return true;
+        }
+        return false;
     }
 
 }
