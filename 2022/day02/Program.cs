da1c580d639fbfce92a0aaf72da71c1f4314f9d2
Matteo Mangioni <mat.mangioni@gmail.com>
2022-12-06 18:50:02 +0100

    Add solution for second star

--- 2022/day02/Program.cs
+++ 2022/day02/Program.cs
@@ -5,28 +5,38 @@ public class Program {
 
     public static void Main(string[] args) {
         
-        int myPoints = 0;
+        int firstStartPoints = 0, secondStarPoints = 0;
 
         while (!isEOF()) {
             string line = Console.ReadLine() + "";
-            myPoints += pointsPerRound(line[0], line[2]);
+            if (line.Length >= 2) {
+                firstStartPoints += pointsForFirstStar(line[0], line[2]);
+                secondStarPoints += pointsForSecondStar(line[0], line[2]);
+            }
         }
 
-        Console.WriteLine("Star1: " + myPoints);
+        Console.WriteLine("Star1: " + firstStartPoints);
+        Console.WriteLine("Star2: " + secondStarPoints);
 
     }
 
-    private static int pointsPerRound(char opponentMove, char myMove) {
+    private static int pointsForFirstStar(char opponentMove, char myMove) {
         int opponentMoveValue = evaluateMove(opponentMove);
         int myMoveValue = evaluateMove(myMove);
 
         int points = myMoveValue + 1;
-        if ((opponentMoveValue + 1) % 3 == myMoveValue) {
+        if (modulus(opponentMoveValue + 1, 3) == myMoveValue) {
             points += 6;
         } else if (myMoveValue == opponentMoveValue) {
             points += 3;
         }
 
+        return points;
+    }
+
+    private static int pointsForSecondStar(char opponentMove, char outcome) {
+        int points = evaluateMove(outcome) * 3;
+        points += modulus(evaluateMove(opponentMove) + offsetByOutcome(outcome), 3) + 1;
         return points;
     }
 
@@ -39,8 +49,17 @@ public class Program {
         return move - reference;
     }
 
+    private static int offsetByOutcome(char outcome)
+    {
+        return outcome - 'Y';
+    }
+
     private static bool isEOF() {
         return Console.In.Peek() == -1;
+    }
+
+    private static int modulus(int dividend, int divisor) {
+        return (dividend % divisor + divisor) % divisor;
     }
 
 }
