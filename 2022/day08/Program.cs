a7b18819da0963bd30d8984adf085d49ff617655
Matteo Mangioni <mat.mangioni@gmail.com>
2022-12-08 10:34:51 +0100

    Add solution to second star

--- 2022/day08/Program.cs
+++ 2022/day08/Program.cs
@@ -4,18 +4,32 @@ namespace day08;
 
 class Program {
 
+    record TreeDirectionPair {
+        public bool visibleFromOutside;
+        public int treesVisibileInDirection;
+
+        public TreeDirectionPair(bool visible, int count) {
+            this.visibleFromOutside = visible;
+            this.treesVisibileInDirection = count;
+        }
+    }
+
     static void Main(string[] args) {
 
         int[][] trees = parseInput();
 
+        int maxScore = 0;
         int visibleTrees = 0;
         for (int i = 0; i < trees.Count(); i++) {
             for (int j = 0; j < trees[i].Count(); j++) {
                 visibleTrees += (isVisible(trees, i, j))? 1 : 0;
+                int score = treeScenicScore(trees, i, j);
+                maxScore = (score > maxScore)? score : maxScore;
             }
         }
 
         Console.WriteLine("Star1: " + visibleTrees);
+        Console.WriteLine("Star2: " + maxScore);
 
     }
 
@@ -39,13 +53,20 @@ class Program {
     }
 
     static bool isVisible(int[][] matrix, int row, int col) {
-        return isVisibileFrom(matrix, row, col, false, false) ||
-               isVisibileFrom(matrix, row, col, false, true)  ||
-               isVisibileFrom(matrix, row, col, true, false)  ||
-               isVisibileFrom(matrix, row, col, true, true);
+        return isVisibileFrom(matrix, row, col, false, false).visibleFromOutside ||
+               isVisibileFrom(matrix, row, col, false, true).visibleFromOutside  ||
+               isVisibileFrom(matrix, row, col, true, false).visibleFromOutside  ||
+               isVisibileFrom(matrix, row, col, true, true).visibleFromOutside;
     }
 
-    static bool isVisibileFrom(int[][] matrix, int row, int col, bool rowOrCol, bool incOrDec) {
+    static int treeScenicScore(int[][] matrix, int row, int col) {
+        return isVisibileFrom(matrix, row, col, false, false).treesVisibileInDirection *
+               isVisibileFrom(matrix, row, col, false, true).treesVisibileInDirection  *
+               isVisibileFrom(matrix, row, col, true, false).treesVisibileInDirection  *
+               isVisibileFrom(matrix, row, col, true, true).treesVisibileInDirection;
+    }
+
+    private static TreeDirectionPair isVisibileFrom(int[][] matrix, int row, int col, bool rowOrCol, bool incOrDec) {
         int tree = matrix[row][col];
         
         int axis = (rowOrCol)? row : col;
@@ -54,11 +75,13 @@ class Program {
         Predicate<int> condition = (incOrDec)? k => (k < maxOnAxis) : k => (k >= 0);
         Predicate<int> visibleIf = (rowOrCol)? k => (matrix[k][col] < tree) : (k) => (matrix[row][k] < tree);
 
+        int count = 0;
         bool visible = true;
         for (int k = axis + update; condition(k) && visible; k += update) {
             visible = visibleIf(k);
+            count++;
         }
 
-        return visible;
+        return new TreeDirectionPair(visible, count);
     }
 }
